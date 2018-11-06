b4463639eea452136c589f0630ad9caa36b7a814
Administrator <Administrator@NWTVOP0OWTDS4XE>
2018-11-06 09:07:29 +0800

    添加项目文件。

---
+++ waterClient/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace waterClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconn = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.tbxMessage = new System.Windows.Forms.RichTextBox();
+            this.tbxPort = new System.Windows.Forms.TextBox();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnconn
+            // 
+            this.btnconn.Location = new System.Drawing.Point(12, 12);
+            this.btnconn.Name = "btnconn";
+            this.btnconn.Size = new System.Drawing.Size(75, 23);
+            this.btnconn.TabIndex = 0;
+            this.btnconn.Text = "连接服务器";
+            this.btnconn.UseVisualStyleBackColor = true;
+            this.btnconn.Click += new System.EventHandler(this.btnconn_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(526, 445);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(494, 10);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 52);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(614, 213);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.Text = "";
+            // 
+            // tbxMessage
+            // 
+            this.tbxMessage.Location = new System.Drawing.Point(12, 271);
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(614, 158);
+            this.tbxMessage.TabIndex = 4;
+            this.tbxMessage.Text = "";
+            // 
+            // tbxPort
+            // 
+            this.tbxPort.Location = new System.Drawing.Point(244, 12);
+            this.tbxPort.Name = "tbxPort";
+            this.tbxPort.Size = new System.Drawing.Size(100, 21);
+            this.tbxPort.TabIndex = 5;
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Location = new System.Drawing.Point(106, 14);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(100, 21);
+            this.tbxIP.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 490);
+            this.Controls.Add(this.tbxIP);
+            this.Controls.Add(this.tbxPort);
+            this.Controls.Add(this.tbxMessage);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnconn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconn;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.RichTextBox tbxMessage;
+        private System.Windows.Forms.TextBox tbxPort;
+        private System.Windows.Forms.TextBox tbxIP;
+    }
+}
+
---
+++ waterClient/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace waterClient
+{
+    public partial class Form1 : Form
+    {
+
+        private Socket sock;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(tbxIP.Text);
+                IPEndPoint myport = new IPEndPoint(ip, Convert.ToInt32(tbxPort.Text));
+                sock.Connect(myport);
+                ShowMsg("连接成功");
+                Thread th = new Thread(Receive);
+                th.IsBackground = true;
+                th.Start(sock);
+            }
+            catch (Exception)
+            {
+                ShowMsg("连接服务器失败。。。请仔细检查服务器是否开启");
+            }
+          
+        }
+        void Receive(object o)
+        {
+            Socket ClientRecieve = o as Socket;
+            ShowMsg("开始接收服务端信息");
+            while (true)
+            {
+                try
+                {
+
+                    byte[] buffer = new byte[1024 * 1024 * 3];
+                    int i = ClientRecieve.Receive(buffer);
+                    if (i == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, i);
+                    ShowMsg(ClientRecieve.RemoteEndPoint + ":" + str);
+
+                }
+                catch(Exception)
+                {
+                    ShowMsg("服务端接收异常");
+                }
+            }
+        }
+      
+        public void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            sock.Close();
+            btnconn.Enabled = true ;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string str = tbxMessage.Text.Trim();
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            sock.Send(buffer);
+            tbxMessage.Clear();
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        }
+        }
+
+
